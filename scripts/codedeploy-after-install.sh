fe67e66effdaa4e67e6ddd0c07c6b7bda19c6dfb
Aaron Carlton <aaron.r.carlton@gmail.com>
2020-05-05 16:48:40 +0200

    moving permissions fix to after-install

--- scripts/codedeploy-after-install.sh
+++ scripts/codedeploy-after-install.sh
@@ -2,5 +2,15 @@
 
 cd /var/www/drupal
 
+# permissions
+# https://www.drupal.org/forum/support/post-installation/2016-09-22/file-and-directory-permissions-lets-finally-get-this
+find /var/www/drupal -type d -exec chmod 755 {} +
+find /var/www/drupal -type f -exec chmod 644 {} +
+chmod /var/www/drupal/.htaccess 444
+chmod /var/www/drupal/sites/default 555
+chmod /var/www/drupal/sites/default/settings.php 400
+find /var/www/drupal/sites/default/files -type d -exec chmod 755 {} +
+find /var/www/drupal/sites/default/files -type f -exec chmod 664 {} +
+
 # TODO: does each application server *always* need to rebuild the cache? CodePipeline custom action?
 ./vendor/bin/drush cache:rebuild
