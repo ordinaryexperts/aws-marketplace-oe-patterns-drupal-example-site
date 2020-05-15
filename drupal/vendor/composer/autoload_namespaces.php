c7046954fa2e9f43711987c2546a1ddb92843d57
Aaron Carlton <aaron.r.carlton@gmail.com>
2020-05-15 17:26:02 +0200

    adding drush with composer

--- drupal/vendor/composer/autoload_namespaces.php
+++ drupal/vendor/composer/autoload_namespaces.php
@@ -11,6 +11,7 @@ return array(
     'PEAR' => array($vendorDir . '/pear/pear_exception'),
     'EasyRdf_' => array($vendorDir . '/easyrdf/easyrdf/lib'),
     'Doctrine\\Common\\Collections\\' => array($vendorDir . '/doctrine/collections/lib'),
+    'Dflydev\\DotAccessData' => array($vendorDir . '/dflydev/dot-access-data/src'),
     'Console' => array($vendorDir . '/pear/console_getopt'),
     'Archive_Tar' => array($vendorDir . '/pear/archive_tar'),
     '' => array($vendorDir . '/pear/pear-core-minimal/src'),
