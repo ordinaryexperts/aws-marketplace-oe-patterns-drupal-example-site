5416e32d446de0b66226988dffd64a63ab0054ab
Aaron Carlton <aaron.r.carlton@gmail.com>
2020-05-15 17:18:58 +0200

    clear cache before permissions

--- scripts/codedeploy-after-install.sh
+++ scripts/codedeploy-after-install.sh
@@ -3,6 +3,15 @@
 # symlink to efs files directory
 ln -s /mnt/efs/drupal/files /var/www/app/drupal/sites/default/files
 
+# TODO: does each application server *always* need to rebuild the cache? CodePipeline custom action?
+cd /var/www/app/drupal
+composer require drush/drush
+connected=$(/var/www/app/drupal/vendor/bin/drush sql:connect)
+if [[ connected == 0 ]]
+then
+    /var/www/app/drupal/vendor/bin/drush cache:rebuild
+fi
+
 # permissions
 # https://www.drupal.org/forum/support/post-installation/2016-09-22/file-and-directory-permissions-lets-finally-get-this
 find /var/www/app/drupal -type d -exec chmod 755 {} +
@@ -14,12 +23,3 @@ chmod 755 /var/www/app/drupal/vendor/drush/drush/drush
 find /var/www/app/drupal/sites/default/files -type d -exec chmod 755 {} +
 find /var/www/app/drupal/sites/default/files -type f -exec chmod 664 {} +
 chown -R www-data /var/www/app/drupal
-
-# TODO: does each application server *always* need to rebuild the cache? CodePipeline custom action?
-cd /var/www/app/drupal
-composer require drush/drush
-connected=$(/var/www/app/drupal/vendor/bin/drush sql:connect)
-if [[ connected == 0 ]]
-then
-    /var/www/app/drupal/vendor/bin/drush cache:rebuild
-fi
